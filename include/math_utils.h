054177a15b5fe14541328529cb1ae397649d230f
Didas72 <diogocruzdiniz@gmail.com>
2024-11-23 23:53:26 +0000

    Added MIN and MAX to math_utils

--- include/math_utils.h
+++ include/math_utils.h
@@ -1,6 +1,8 @@
 #ifndef SUS_MATH_UTILS_H_
 #define SUS_MATH_UTILS_H_
 
+#define MAX(a,b) ((a) < (b) ? (b) : (a))
+#define MIN(a,b) ((a) > (b) ? (b) : (a))
 #define DIV_CEIL(a, b) ((((a) + (b) - 1) / (b)))
 
 #endif
